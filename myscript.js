44656fe187854b4d852793c2caff726ed9a2e8e1
SHEFFcode <sheff@sheffmachine.com>
2016-07-28 15:26:52 -0700

    add some safety checks

--- myscript.js
+++ myscript.js
@@ -10,7 +10,8 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     $('html').append(script);
     break;
     case 'browser-replay':
-    location.href=location.href.replace(/(.*?)(id\/)([a-z0-9-]*)(.*)/, "https://replay.replay.answerscloud.com/replay/replay?browser_mode=CHROME&session_id=$3&page_number=0&player=web&speed=1.00");
+    console.log('location change requested');
+    location.href='google.com';
     break;
   }
 });
--- popup.js
+++ popup.js
@@ -1,37 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
   var injecthttp = document.getElementById('injecthttp');
   // onClick's logic below:
-  injecthttp.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log('button clicked');
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {message: "injectHTTP"});
+  if (injecthttp) {
+    injecthttp.addEventListener('click', function(e) {
+      e.preventDefault();
+      console.log('button clicked');
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {message: "injectHTTP"});
+      });
     });
-  });
+  }
+
   var injecthttps = document.getElementById('injecthttps');
-  // onClick's logic below:
-  injecthttps.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log('button clicked');
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {message: "injectHTTPS"});
+  if (injecthttps) {
+    // onClick's logic below:
+    injecthttps.addEventListener('click', function(e) {
+      e.preventDefault();
+      console.log('button clicked');
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {message: "injectHTTPS"});
+      });
     });
-  });
+  }
 
   var browserReplay = document.getElementById('browser-replay');
   // onClick's logic below:
-  browserReplay.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log('button clicked');
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {message: "browser-replay"});
+  if (browserReplay) {
+    browserReplay.addEventListener('click', function(e) {
+      e.preventDefault();
+      console.log('button clicked');
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {message: "browser-replay"});
+      });
     });
-  });
+  }
 
-  $(document).ready(function(){
-    $('body').on('click', '#exernal', function(){
+  var external = document.getElementById('external');
+  // onClick's logic below:
+  if (external) {
+    external.addEventListener('click', function(e) {
+      console.log('clicked');
       chrome.tabs.create({url: $(this).attr('href')});
-      return false;
     });
-  });
+  }
+  
 });
