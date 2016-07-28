370bea4da1d957a65cdf41131c4ce2d3142b6f55
SHEFFcode <sheff@sheffmachine.com>
2016-07-28 16:22:26 -0700

    additional improvements to the code

--- myscript.js
+++ myscript.js
@@ -3,11 +3,11 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   switch (message.message) {
     case 'injectHTTP':
     var script = '<script src="http://localhost:8080/gateway.min.js"></script>';
-    $('html').append(script);
+    $('head').append(script);
     break;
     case 'injectHTTPS':
     var script = '<script src="https://localhost/gateway.min.js"></script>';
-    $('html').append(script);
+    $('head').append(script);
     break;
     case 'browser-replay':
     location.href=location.href.replace(/(.*?)(id\/)([a-z0-9-]*)(.*)/, "https://replay.replay.answerscloud.com/replay/replay?browser_mode=CHROME&session_id=$3&page_number=0&player=web&speed=1.00");
