1850e1c3a579c1ce8fb4e34aecf3da7a0f842457
SHEFFcode <sheff@sheffmachine.com>
2016-07-28 23:49:38 -0700

    wrap up for hackathon

--- myscript.js
+++ myscript.js
@@ -4,10 +4,18 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     case 'injectHTTP':
     var script = '<script src="http://localhost:8080/gateway.min.js"></script>';
     $('head').append(script);
+    chrome.storage.sync.set({'http': true}, function() {
+      // Notify that we saved.
+      message('Settings saved');
+    });
     break;
     case 'injectHTTPS':
     var script = '<script src="https://localhost/gateway.min.js"></script>';
     $('head').append(script);
+    chrome.storage.sync.set({'https': true}, function() {
+      // Notify that we saved.
+      message('Settings saved');
+    });
     break;
     case 'browser-replay':
     location.href=location.href.replace(/(.*?)(id\/)([a-z0-9-]*)(.*)/, "https://replay.replay.answerscloud.com/replay/replay?browser_mode=CHROME&session_id=$3&page_number=0&player=web&speed=1.00");
@@ -15,5 +23,21 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     case 'showinvite':
     FSR.showInvite();
     break;
+  }
+});
+
+chrome.storage.sync.get('http', function (obj) {
+  console.log(JSON.stringify(obj.http));
+  if (obj.http === true) {
+    var script = '<script src="http://localhost:8080/gateway.min.js"></script>';
+    $('head').append(script);
+  }
+});
+
+chrome.storage.sync.get('https', function (obj) {
+  console.log(JSON.stringify(obj.http));
+  if (obj.http === true) {
+    var script = '<script src="https://localhost/gateway.min.js"></script>';
+    $('head').append(script);
   }
 });
