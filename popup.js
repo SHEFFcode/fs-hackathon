9787870b33ce5075738c236ac87ed379b8c81853
SHEFFcode <sheff@sheffmachine.com>
2016-08-01 13:23:12 -0700

    update to fix the video not converting to html5 bug

--- popup.js
+++ popup.js
@@ -3,41 +3,44 @@ document.addEventListener('DOMContentLoaded', function() {
   /*==========  HTTP Injection ============ */
   var injecthttp = document.getElementById('injecthttp');
   // onClick's logic below:
-  injecthttp.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log('button clicked');
-    if (injecthttp) {
+  if (injecthttp) {
+    injecthttp.addEventListener('click', function(e) {
+      e.preventDefault();
+      console.log('button clicked');
+
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {message: "injectHTTP"});
       });
-    }
-  });
+    });
+  };
 
   /*==========  HTTPS Injection ============ */
   var injecthttps = document.getElementById('injecthttps');
   // onClick's logic below:
-  injecthttps.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log('button clicked');
-    if (injecthttps) {
+  if (injecthttps) {
+    injecthttps.addEventListener('click', function(e) {
+      e.preventDefault();
+      console.log('button clicked');
+
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {message: "injectHTTPS"});
       });
-    }
-  });
+    });
+  };
 
   /*==========  Stop Injection ============ */
   var stopinjection = document.getElementById('stopinjection');
-  // onClick's logic below:
-  stopinjection.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log('button clicked');
-    if (injecthttps) {
+  if (injecthttps) {
+    // onClick's logic below:
+    stopinjection.addEventListener('click', function(e) {
+      e.preventDefault();
+      console.log('button clicked');
+
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {message: "stopInjection"});
       });
-    }
-  });
+    })
+  };
 
   var browserReplay = document.getElementById('browser-replay');
   // onClick's logic below:
